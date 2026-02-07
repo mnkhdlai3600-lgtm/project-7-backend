a26520e328f45cd9b79f031776f5b2449f563a3a
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-02-07 15:16:58 +0800

    email otp

--- src/utils/reset-password-nodemailer.utils.ts
+++ src/utils/reset-password-nodemailer.utils.ts
@@ -1,38 +1,37 @@
-import nodemailer from "nodemailer";
-import { configDotenv } from "dotenv";
+import { Resend } from "resend";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-configDotenv();
+const { RESEND_API_KEY, AUTH_EMAIL } = process.env;
 
-const { AUTH_EMAIL, AUTH_PASS } = process.env;
-
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: AUTH_EMAIL,
-    pass: AUTH_PASS,
-  },
-});
+const resend = new Resend(RESEND_API_KEY);
 
 export const ResetPasswordVerificationEmail = async (
   reciever: string,
   otpCode: string,
 ) => {
-  await transport.sendMail({
-    from: `"Food Delivery Team" <${AUTH_EMAIL}>`,
-    to: reciever,
-    subject: "Нууц үг сэргээх баталгаажуулах код",
-    html: `
-    <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f4f7f6;">
-      <div style="max-width: 500px; margin: auto; background: white; padding: 40px; border-radius: 15px;">
-        <h2 style="color: #333;">Нууц үг сэргээх</h2>
-        <p>Таны баталгаажуулах код:</p>
-        <div style="font-size: 32px; font-weight: bold; color: #007bff; letter-spacing: 5px; padding: 20px; border: 2px dashed #007bff; display: inline-block;">
-          ${otpCode}
+  try {
+    const { data, error } = await resend.emails.send({
+      from: `Food Delivery Team <${AUTH_EMAIL}>`,
+      to: reciever,
+      subject: "Нууц үг сэргээх баталгаажуулах код",
+      html: `
+      <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; background-color: #f4f7f6;">
+        <div style="max-width: 500px; margin: auto; background: white; padding: 40px; border-radius: 15px;">
+          <h2 style="color: #333;">Нууц үг сэргээх</h2>
+          <p>Таны баталгаажуулах код:</p>
+          <div style="font-size: 32px; font-weight: bold; color: #007bff; letter-spacing: 5px; padding: 20px; border: 2px dashed #007bff; display: inline-block;">
+            ${otpCode}
+          </div>
+          <p style="color: #888; margin-top: 20px;">Энэ код 10 минутын дараа хүчингүй болно.</p>
         </div>
-        <p style="color: #888; margin-top: 20px;">Энэ код 10 минутын дараа хүчингүй болно.</p>
       </div>
-    </div>
-    `,
-  });
+      `,
+    });
+
+    if (error) return console.error("Resend OTP Error:", error);
+    console.log("Reset Password Email Sent:", data);
+  } catch (err) {
+    console.error("Catch Error:", err);
+  }
 };
-export default transport;
