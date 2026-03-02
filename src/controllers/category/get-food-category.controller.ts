516bf8bf2482add44aa9a611fb271b820c0fc497
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-02 09:48:49 +0800

    final

--- src/controllers/category/get-food-category.controller.ts
+++ src/controllers/category/get-food-category.controller.ts
@@ -1,15 +1,13 @@
 import { Response, Request } from "express";
 import foodCategoryModel from "../../schema/foodCategory.model";
 
-// Энэ функц баазад байгаа БҮХ категорийг авчирна
 export const getAllFoodCategories = async (req: Request, res: Response) => {
   try {
-    // find({}) гэвэл бүх өгөгдлийг шүүж авна
-    const allCategories = await foodCategoryModel.find({}).populate("foodIds"); // Таны Compass дээр foodId байвал "foodId" гэж засаарай
+    const allCategories = await foodCategoryModel.find({}).populate("foodIds");
 
     res.status(200).json({
       success: true,
-      data: allCategories, // Энэ нь [ {Appetizers}, {Salads}, ... ] гэсэн массив байна
+      data: allCategories,
     });
   } catch (error) {
     res.status(500).json({
