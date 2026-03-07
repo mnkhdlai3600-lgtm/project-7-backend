a59aad4a05b2266ec30fb2be6daedf8426260660
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-07 15:12:27 +0800

    test

--- src/router/authentication.router.ts
+++ src/router/authentication.router.ts
@@ -31,7 +31,7 @@ authenticationRouter.delete(
   authorization(userRoles.Admin),
   deleteUser,
 );
-authenticationRouter.get("/verify-email", verifyEmailController);
+authenticationRouter.get("/authentication/verify-email", verifyEmailController);
 authenticationRouter.put(
   "/update-user/:email",
   authentication,
