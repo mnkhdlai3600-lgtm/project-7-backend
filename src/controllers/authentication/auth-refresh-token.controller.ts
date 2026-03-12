b49825c71c6d278ba98bca3206811f33ccdd1418
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-12 09:08:52 +0800

    ended

--- src/controllers/authentication/auth-refresh-token.controller.ts
+++ src/controllers/authentication/auth-refresh-token.controller.ts
@@ -3,31 +3,39 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export const refreshToken = (req: Request, res: Response) => {
-  const { token: refreshToken } = req.query;
+export const refreshTokenController = (req: Request, res: Response) => {
+  const { refreshToken } = req.body;
 
   if (!refreshToken) {
-    res.status(401).json({ message: "Token!" });
+    res.status(401).json({ message: "Refresh token байхгүй байна." });
     return;
   }
 
   try {
-    const decodedToken = jwt.verify(refreshToken as string, JWT_SECRET) as {
+    const decodedToken = jwt.verify(refreshToken, JWT_SECRET) as {
       email: string;
+      _id?: string;
     };
 
-    const newToken = jwt.sign({ email: decodedToken.email }, JWT_SECRET, {
-      expiresIn: "7d",
-    });
+    const accessToken = jwt.sign(
+      {
+        email: decodedToken.email,
+        _id: decodedToken._id,
+      },
+      JWT_SECRET,
+      { expiresIn: "7d" },
+    );
 
     res.status(200).json({
       success: true,
-      token: newToken,
+      accessToken,
       user: {
         email: decodedToken.email,
       },
     });
   } catch (error) {
-    res.status(403).json({ message: "Token expired or invalid!" });
+    res
+      .status(403)
+      .json({ message: "Refresh token хүчингүй эсвэл хугацаа дууссан байна." });
   }
 };
