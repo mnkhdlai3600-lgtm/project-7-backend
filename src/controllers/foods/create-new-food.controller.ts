8cd6a63a6220655bf965d5e18291df5fa891a879
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-13 09:59:37 +0800

    food schema

--- src/controllers/foods/create-new-food.controller.ts
+++ src/controllers/foods/create-new-food.controller.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
 import FoodModel from "../../schema/food.model";
+import FoodCategoryModel from "../../schema/foodCategory.model";
 
 export const createNewFood = async (req: Request, res: Response) => {
   try {
-    const newFood = await FoodModel.create(req.body);
+    const { foodName, price, image, ingredients, categoryId } = req.body;
+
+    if (!foodName || !price || !image || !ingredients || !categoryId) {
+      res.status(400).json({
+        success: false,
+        message: "Бүх талбарыг бөглөнө үү.",
+      });
+      return;
+    }
+
+    const newFood = await FoodModel.create({
+      foodName,
+      price,
+      image,
+      ingredients,
+      categoryId,
+    });
+
+    await FoodCategoryModel.findByIdAndUpdate(categoryId, {
+      $push: { foodIds: newFood._id },
+    });
 
     res.status(201).json({
       success: true,
