584815808948264312f2c90d4ceceb1999173db4
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-13 09:32:31 +0800

    food schema

--- src/schema/food.model.ts
+++ src/schema/food.model.ts
@@ -2,10 +2,15 @@ import { Schema, model, models } from "mongoose";
 
 const FoodSchema = new Schema(
   {
-    name: { type: String, required: true, trim: true },
+    foodName: { type: String, required: true, trim: true },
     price: { type: Number, required: true },
     image: { type: String, required: true },
-    ingredients: [String],
+    ingredients: { type: String, required: true },
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: "food-categories",
+      required: true,
+    },
     isAvailable: { type: Boolean, default: true },
   },
   { timestamps: true },
