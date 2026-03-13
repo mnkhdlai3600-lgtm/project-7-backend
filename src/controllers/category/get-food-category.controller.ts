19edbed6368244eb41af68e50f1a85605092eb52
mnkhdlai3600-lgtm <236936562+mnkhdlai3600-lgtm@users.noreply.github.com>
2026-03-13 11:36:53 +0800

    food schema

--- src/controllers/category/get-food-category.controller.ts
+++ src/controllers/category/get-food-category.controller.ts
@@ -1,13 +1,28 @@
 import { Response, Request } from "express";
 import foodCategoryModel from "../../schema/foodCategory.model";
+import FoodModel from "../../schema/food.model";
 
 export const getAllFoodCategories = async (req: Request, res: Response) => {
   try {
-    const allCategories = await foodCategoryModel.find({}).populate("foodIds");
+    const allCategories = await foodCategoryModel.find({});
+
+    const categoriesWithCount = await Promise.all(
+      allCategories.map(async (category) => {
+        const count = await FoodModel.countDocuments({
+          categoryId: category._id,
+        });
+
+        return {
+          _id: category._id,
+          categoryName: category.categoryName,
+          count,
+        };
+      }),
+    );
 
     res.status(200).json({
       success: true,
-      data: allCategories,
+      data: categoriesWithCount,
     });
   } catch (error) {
     res.status(500).json({
